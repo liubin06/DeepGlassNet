989c0fb229e3bae4690200747291069dee286dfa
Liu Bin <269336620@qq.com>
2025-02-07 21:42:06 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,50 +1,68 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-class DeepGlass(nn.Module):
-    def __init__(self, input_dim=19, hidden_dim1=64, hidden_dim2=32, output_dim=16):
-        super(DeepGlass, self).__init__()
-
-        # 交叉特征提取层: 是两层线性变换层，分别用于将输入特征映射到隐层。首先将输入的19维特征映射到128维，然后再进一步映射到64维。这两层可以通过简单的线性操作隐式地学习输入特征之间的交互关系。
-        self.linear1 = nn.Linear(input_dim, hidden_dim1)
-        self.linear2 = nn.Linear(hidden_dim1, hidden_dim2)
-
-        # 自注意力层
-        self.attention = nn.MultiheadAttention(embed_dim=hidden_dim2, num_heads=1)  # 注意力模块
-        # 全连接层
-        self.fc1 = nn.Linear(hidden_dim2, hidden_dim1)
-        self.fc2 = nn.Linear(hidden_dim1, output_dim)
-
-        # 批归一化和Dropout
-        self.bn1 = nn.BatchNorm1d(hidden_dim1)
-        self.dropout = nn.Dropout(p=0.5)
-
-    def forward(self, x):
-        # 线性变换层
-        x = F.tanh(self.linear1(x))
-        x = F.tanh(self.linear2(x))
-
-        # # 自注意力机制
-        # x = x.unsqueeze(0)  # 增加一个维度，变成 [1, batch_size, hidden_dim2]
-        # attn_output, _ = self.attention(x, x, x)
-        # x = attn_output.squeeze(0)  # 恢复维度，变成 [batch_size, hidden_dim2]
-
-        # 全连接层1
-        x = F.relu(self.bn1(self.fc1(x)))
-        x = self.dropout(x)
-
-        # 全连接层2
-        x = self.fc2(x)
-        l2_norm = torch.norm(x, p=2, dim=1, keepdim=True)
-
-        return x/l2_norm
+torch.autograd.set_detect_anomaly(True)
+USE_CUDA = torch.cuda.is_available()
+device = torch.device('cuda' if USE_CUDA else 'cpu')
 
 
-# # # 示例：创建模型并进行前向传播
-# model = DeepGlass()
-# sample_input = torch.rand(32, 19)  # 32个样本，每个样本19个特征
-# print(sample_input.shape)
-# output = model(sample_input)
-# print(output.shape)  # 输出应为 [32, 1]
-#
+class DeepGlassNet(nn.Module):
+    def __init__(self, num_components, embedding_dim, fm_dim,num_heads, out_dim,dropout_rate=0.1):
+        super(DeepGlassNet, self).__init__()
+        self.num_components = num_components
+        self.embedding_dim = embedding_dim
+        self.fm_dim = fm_dim
+        self.num_heads = num_heads
+        self.out_dim = out_dim
+
+        self.dropout_rate = dropout_rate
+
+        # 1. Each component is parameterized with a learnable embedding vector
+        self.component_embeddings = nn.Parameter(torch.randn(num_components,embedding_dim))
+        nn.init.xavier_uniform_(self.component_embeddings)
+
+        # 2. A low-rank matrix is used to parameterize the adjacency matrix that governs interactions between components.
+        self.v = nn.Parameter(torch.randn(self.num_components, self.fm_dim))
+        nn.init.xavier_uniform_(self.v)
+
+        # 3. Self attention
+        self.attention_layer = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=self.num_heads)
+
+        # 4. Projection head
+        self.projector_layer = nn.Sequential(
+                                        nn.Linear(self.num_components*self.embedding_dim, 1024, bias =False),
+                                        nn.BatchNorm1d(1024),
+                                        nn.ReLU(),
+                                        nn.Dropout(p=self.dropout_rate),
+                                        nn.Linear(1024, self.out_dim,bias=True)
+        )
+
+    def forward(self, component_proportion):
+        '''
+        :param component_proportion: [bs, num_components]
+        :return: feature representations: [bs, out_dim]
+        '''
+        # 1. Proportion Modulated Embedding Layer
+        batch_size = component_proportion.shape[0]
+        component_proportion = component_proportion.unsqueeze(-1).repeat(1, 1, self.embedding_dim) #[bs, num_components, embedding_dim]
+        fc_1 = component_proportion * self.component_embeddings.unsqueeze(0)      # [bs * num_components, embedding_dim ]
+
+
+        # 2. Graph Convolution Layer
+        norm_v = F.normalize(self.v,dim=-1)
+        adj_matrix = torch.matmul(norm_v, norm_v.T)
+        # adj_matrix = torch.matmul(self.v, self.v.T)
+        adj_matrix1 = adj_matrix.unsqueeze(0).expand(batch_size,-1,-1)      # [bs * num_components * num_components]
+        cov_layer = torch.matmul(adj_matrix1,fc_1)                          # [bs * num_components * embedding_dim]
+        fc_2 = fc_1 + cov_layer/(self.num_components-1)
+
+        # 3. Self-Attention Layer
+        fc_2 = fc_2.permute(1,0,2)                                   # [num_component, bs, embedding_dim]
+        fc_3, attention_weight = self.attention_layer(fc_2,fc_2,fc_2)       # [num_component, bs, embedding_dim]
+        fc_3 = fc_3.transpose(0,1).contiguous().view(batch_size,-1)        # [bs, num_componen * embedding_dim]
+
+        # 4. Nonlinear Projection Layer
+        fc_4 = self.projector_layer(fc_3)
+
+        return F.normalize(fc_4,dim=-1)
+
