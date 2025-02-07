3a027595cde6ae7af914cd2623ab8a4243aae1e9
Liu Bin <269336620@qq.com>
2025-02-07 21:55:16 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,61 +9,85 @@ from torch.utils.data import Dataset, DataLoader
 def load_train(data_path):
     '''
     :param ：train data path
-    :return: ndarray with shape (n_samples, n_features + 1 property)
+    :return: ndarray 
     '''
     data = pd.read_csv(data_path,
                        header=None,
                        sep=',',
                        encoding='utf-8')
-    print('Number of training samples: {}, Number of components: {}'.format(data.shape[0], data.shape[1] - 1))
+    print('Number of training samples: {}'.format(data.shape[0]))
     return np.array(data)
 
 
 def load_validate(data_path):
     '''
     :param ：validate data path
-    :return: ndarray with shape (n_samples, n_components + 1 property)
+    :return: ndarray 
     '''
     data = pd.read_csv(data_path,
                        header=None,
                        sep=',',
                        encoding='utf-8')
-    print('Number of testing samples: {}, Number of components: {}'.format(data.shape[0], data.shape[1]-1))
+    print('Number of validating samples: {}'.format(data.shape[0]))
+    return np.array(data)
+
+def load_test(data_path):
+    '''
+    :param ：validate data path
+    :return: ndarray 
+    '''
+    data = pd.read_csv(data_path,
+                       header=None,
+                       sep=',',
+                       encoding='utf-8')
     return np.array(data)
 
 
 class MyData(Dataset):
-    def __init__(self, data, input_dim, train=True):
+    def __init__(self, data, mean, std, input_dim, interval, noise_std, phase):
         '''
         :param data: ndarray with shape (n_samples, n_features+1 property)
         :param input_dim: number of input features (components)
         :param train: boolean indicating for loading training set or validation set
+        :param std: the standard deviation of the noise for data augmentation
+        :param interval: the interval of glass transition temperatures to be selected
         '''
         self.input_dim = input_dim
         self.data = torch.tensor(data[:, :self.input_dim],dtype=torch.float32)
-        self.GT = torch.tensor(data[:, self.input_dim],dtype=torch.float32)
-        self.label = torch.tensor([ 500 <= self.GT[id] <= 600 for id in range(len(self))]).float()
-        self.train = train
-        self.posidx = [id for id in range(len(self)) if self.label[id] == 1.]
-        self.negidx = [id for id in range(len(self)) if self.label[id] == 0.]
+        self.interval = interval
+        self.noise_std = noise_std
+        self.phase = phase
+        self.mean = torch.tensor(mean,dtype=torch.float32)
+        self.std= torch.tensor(std,dtype=torch.float32)
+        
+        if self.phase != 'Screening':
+            self.GT = torch.tensor(data[:, self.input_dim],dtype=torch.float32)
+            self.label = torch.tensor([ self.interval[0]<= self.GT[id] <= self.interval[1] for id in range(len(self))]).float()
+            self.valid_id = [id for id in range(len(self)) if self.label[id] == 1.]
+            self.invalid_id = [id for id in range(len(self)) if self.label[id] == 0.] 
+        
 
     def __getitem__(self, idx):
-        feature = self.data[idx]
-        label = self.label[idx]
-        if self.train:
-            if label:
-                pos = self.data[random.choice(self.posidx)]
-                neg = self.data[random.choice(self.negidx)]
+        if self.phase == 'Training':
+            feature = self.data[idx]
+            label = self.label[idx]
+            if label==1.:
+                pos = self.data[np.random.choice(self.valid_id)]
+                neg = self.data[np.random.choice(self.invalid_id)]
             else:
-                pos = self.data[random.choice(self.negidx)]
-                neg = self.data[random.choice(self.posidx)]
-            feature, pos, neg = self.perbulation(1, 0.001, feature), self.perbulation(1, 0.001, pos), self.perbulation(1,
-                                                                                                                     0.001,
-                                                                                                                     neg)
+                pos = self.data[np.random.choice(self.invalid_id)]
+                neg = self.data[np.random.choice(self.valid_id)]
+            feature, pos, neg  = self.perbulation(1, self.noise_std, feature),self.perbulation(1, self.noise_std, pos),self.perbulation(1, self.noise_std, neg)
             return self.normalize(feature), self.normalize(pos), self.normalize(neg)
-        else:
-            feature = self.normalize(feature)
-            return feature,label
+        
+        elif self.phase == 'Evaluation':
+            feature = self.data[idx]
+            label = self.label[idx]
+            return self.normalize(feature),label
+        
+        elif self.phase == 'Screening':
+            feature = self.data[idx]
+            return self.normalize(feature)
 
     def __len__(self):
         return len(self.data)
@@ -77,6 +101,8 @@ class MyData(Dataset):
         '''
         perb = torch.normal(miu, sigma, [self.input_dim])
         augmentation = feature * perb
+
+
         return augmentation
 
     def normalize(self, feature):
@@ -84,15 +110,8 @@ class MyData(Dataset):
         :param feature: input feature of dimension 19
         :return: normalized feature with summation of all components equal to 1
         '''
-        normalized_feature = feature #/ sum(feature)
+        normalized_feature = (feature-self.mean)/self.std
         return normalized_feature
 
 
-
-
-# data = load_train('./data/train_tg.csv')
-# traindata = MyData(data, 19,train=True)
-# testdata = MyData(data, 19,train=False)
-# train_loader = DataLoader(traindata, batch_size=2)
-# test_loader = DataLoader(testdata, batch_size=2)
 
